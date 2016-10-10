b7da12e988ad374ebb1e9d1953191a26ce465a0e
rahul sah <rahul.sah089@gmail.com>
2016-10-10 23:14:44 +0530

    Changes made to the reaulr expression1

---
+++ CoreJavaPractice/src/reguarExpression/Example.java
@@ -0,0 +1,10 @@
+package reguarExpression;
+
+public class Example {
+	
+	public static void main(String[] args){
+		System.out.println("Main Method Started");
+		System.out.println("Main Method Ended");
+	}
+
+}
